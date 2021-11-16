181d2f00d017cdd45aa1a80cfd1ac85e9a7b3b27
rikucherry1993 <rikucherry0522@gmail.com>
2021-11-17 00:23:33 +0900

    Create a blank activity to receive auth code

---
+++ app/src/main/java/com/rikucherry/artworkespresso/feature_authentication/view/CallbackActivity.kt
@@ -0,0 +1,37 @@
+package com.rikucherry.artworkespresso.feature_authentication.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.rikucherry.artworkespresso.common.theme.ArtworkEspressoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CallbackActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtworkEspressoTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Callback Success")
+                    }
+                }
+            }
+        }
+    }
+}
