fd0814d7044755ccdc893fb5b3e584d6ef9c530a
rikucherry1993 <rikucherry0522@gmail.com>
2021-11-21 23:54:02 +0900

    Resolve continuous requesting

--- app/src/main/java/com/rikucherry/artworkespresso/feature_authentication/view/CallbackActivity.kt
+++ app/src/main/java/com/rikucherry/artworkespresso/feature_authentication/view/CallbackActivity.kt
@@ -1,9 +1,9 @@
 package com.rikucherry.artworkespresso.feature_authentication.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,20 +12,22 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
 import com.rikucherry.artworkespresso.ArtworkEspressoApplication
 import com.rikucherry.artworkespresso.common.theme.ArtworkEspressoTheme
+import com.rikucherry.artworkespresso.feature_authentication.domain.util.AuthenticationUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CallbackActivity : ComponentActivity() {
 
-    private val viewModel : CallbackActivityViewModel by lazy {
-        ViewModelProvider(this).get(CallbackActivityViewModel::class.java)
-    }
+    private val viewModel : CallbackActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        val authCode = AuthenticationUtil.retrieveAuthorizeCode(intent,(application as ArtworkEspressoApplication).state)
+        viewModel.getAccessToken(authCode)
+
         setContent {
             ArtworkEspressoTheme {
                 Surface(
@@ -37,8 +39,6 @@ class CallbackActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center
                     ) {
                         Text(text = "Callback Success")
-                        viewModel.getAccessToken(intent, (application as ArtworkEspressoApplication).state)
-                        Log.d("viewModel is called", "called")
                     }
                 }
             }
--- app/src/main/java/com/rikucherry/artworkespresso/feature_authentication/view/CallbackActivityViewModel.kt
+++ app/src/main/java/com/rikucherry/artworkespresso/feature_authentication/view/CallbackActivityViewModel.kt
@@ -1,6 +1,5 @@
 package com.rikucherry.artworkespresso.feature_authentication.view
 
-import android.content.Intent
 import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
@@ -8,7 +7,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rikucherry.artworkespresso.common.ResponseHandler
 import com.rikucherry.artworkespresso.feature_authentication.domain.use_case.UserLoginUseCase
-import com.rikucherry.artworkespresso.feature_authentication.domain.util.AuthenticationUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -22,14 +20,12 @@ class CallbackActivityViewModel @Inject constructor(
     private val _state = mutableStateOf("")
     val state: State<String> = _state
 
-    fun getAccessToken(intent: Intent? = null, state: String) {
-        val authCode = AuthenticationUtil.retrieveAuthorizeCode(intent, state) ?: return
-
-        userLoginUseCase(authCode).onEach { result ->
+    fun getAccessToken(authCode: String?) {
+        userLoginUseCase(authCode ?: "").onEach { result ->
             when (result) {
                 is ResponseHandler.Success -> {
                     _state.value = result.data.toString()
-                    Log.d("Auth state: ${result.message} \n data: ", _state.value)
+                    Log.d("Auth state:", _state.value)
                 }
 
                 is ResponseHandler.Loading -> {
