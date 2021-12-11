590916982efac8727caecab5d159963dd5767191
rikucherry1993 <rikucherry0522@gmail.com>
2021-12-11 20:49:00 +0900

    Retrieve images for topic selection page

--- app/src/main/java/com/rikucherry/artworkespresso/feature_topic_selection/presentation/TopTopicsScreen.kt
+++ app/src/main/java/com/rikucherry/artworkespresso/feature_topic_selection/presentation/TopTopicsScreen.kt
@@ -1,15 +1,20 @@
 package com.rikucherry.artworkespresso.feature_topic_selection.presentation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.rikucherry.artworkespresso.R
 import com.rikucherry.artworkespresso.common.component.LineLoader
 import com.rikucherry.artworkespresso.common.theme.Purple100
 import com.rikucherry.artworkespresso.feature_topic_selection.presentation.viewmodel.TopicSelectViewModel
@@ -20,18 +25,44 @@ fun TopTopicsScreen(
 ) {
     val state = viewModel.state.value
 
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        //TODO: Add font
+        Text(
+            fontSize = 32.sp,
+            text = "MENU",
+            modifier = Modifier.padding(16.dp),
+            color = MaterialTheme.colors.primary
+        )
+        Text(
+            fontSize = 24.sp,
+            text = "Follow up to 3 topics",
+            modifier = Modifier.padding(16.dp)
+        )
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
             items(state.data ?: emptyList()) { result ->
-                Text(result.name)
-                //TODO: use glide to retrieve image
-                Text(result.exampleDeviations?.get(0)?.url ?: "")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = result.exampleDeviations?.get(0)?.content?.src ?: "",
+                            builder = {
+                                transformations(CircleCropTransformation())
+                                placeholder(drawableResId = R.drawable.placeholder_topics)
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.size(128.dp)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(result.name)
+                }
             }
         }
 
--- app/src/main/java/com/rikucherry/artworkespresso/feature_topic_selection/presentation/TopicSelectionActivity.kt
+++ app/src/main/java/com/rikucherry/artworkespresso/feature_topic_selection/presentation/TopicSelectionActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.rikucherry.artworkespresso.ArtworkEspressoApplication
@@ -57,8 +56,6 @@ class TopicSelectionActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center
                     ) {
                         val loginState = loginViewModel.state.value
-
-                        Text("This is topic selection")
                         when {
                             loginState.isLoading -> {
                                 LineLoader(
@@ -76,7 +73,6 @@ class TopicSelectionActivity : ComponentActivity() {
                             }
 
                             else -> {
-                                //TODO: Navigate to a list screen
                                 TopTopicsScreen()
                             }
                         }
